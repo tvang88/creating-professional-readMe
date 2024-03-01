b7a7705028ff37f319636e69dfd77a3b61a682ee
unknown <tubvaaj2788@gmail.com>
2024-03-01 01:35:06 -0600

    added inquirer

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-const licensesDetails = require('./Develop/utils/licensesDetails.js')
+const licensesDetails = require('./licensesDetails.js');
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
